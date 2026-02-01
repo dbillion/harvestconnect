c01cca2697c743c2f4b4a9562d80011ef3f0bd82
Oludayo Adeoye <dayozoe@gmail.com>
2026-02-01 03:05:12 -0500

    Switch to Docker Hub image and use declarative koyeb.yaml deployment

--- harvestconnect/components/ui/calendar.tsx
+++ harvestconnect/components/ui/calendar.tsx
@@ -57,7 +57,7 @@ function Calendar({
         ChevronLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         ChevronRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       } as any}
-      ...props
+      {...props}
     />
   )
 }
